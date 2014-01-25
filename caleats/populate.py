07b2b1ce8f2e51ed6c313a3970a11484b8943918
Michael Cheng <michaelcheng@pioneers.berkeley.edu>
2014-01-25 03:54:47 -0800

    Added populate_day, changed MenuItem

---
+++ caleats/populate.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup, Comment
+import urllib2
+from caleats.models import Entree, MenuItem
+
+def get_entree_if_exists(itemname):
+    try:
+        entree = Entree.objects.get(name = itemname)
+    except: #DoesNotExist
+        entree = Entree(name = itemname)
+        entree.save()
+    return entree
+
+def populate_day():
+    MenuItem.objects.all().delete()
+
+    response = urllib2.urlopen('http://services.housing.berkeley.edu/FoodPro/dining/static/todaysentrees.asp')
+    html = response.read()
+
+    #trash.py - go for <!-- -->
+    content = BeautifulSoup(html, 'html5lib').find_all(id = 'content')[2].tbody.find_all(
+        'tr', {"valign":"top"})
+
+    LOCATIONS = {0: "Crossroads", 1: "Cafe 3", 2: "Foothill", 3: "Clark Kerr"}
+    #FOODTYPE = {u'#800040': 'Vegan', u'#008000': 'Vegetarian', u'#000000': 'Regular'}
+    #more than one vegetarian color o__O
+
+    for time in content: #b/l/d
+        for l, loc in enumerate(time.find_all('td')): #cr/c3/fh/ck
+            meal = unicode(loc.find('b').string) #meals bolded
+            if not loc.find('i'):                #closed italicized, no items = closed
+                entrees = loc.find_all('a')      #all meals linked to nutrition
+                for entree in entrees:
+                    name = unicode(entree.string)
+                    #ftype = FOODTYPE[unicode(entree.font['color'])]
+                    dininghall = LOCATIONS[l]
+                    ent = get_entree_if_exists(name)
+                    newitem = MenuItem(entree = ent, hall = dininghall, meal = meal)
+                    newitem.save()
