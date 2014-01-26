13a6985cd7dfea76c88ce82c25bad9a2d14d0130
Ravi Tadinada <discursor@gmail.com>
2014-01-26 01:29:44 -0800

    Fix votes ordering

--- caleats/views.py
+++ caleats/views.py
@@ -13,9 +13,9 @@ def index(request):
 def detail(request, hall):
     hall = hall.lower()
     menuitems = MenuItem.objects.filter(hall = hall)
-    br_menuitems = menuitems.filter(meal = "Breakfast").order_by('-entree.votes')
-    lu_menuitems = menuitems.filter(meal = "Lunch").order_by('-entree.votes')
-    di_menuitems = menuitems.filter(meal = "Dinner").order_by('-entree.votes')
+    br_menuitems = menuitems.filter(meal = "Breakfast").order_by('-entree__votes')
+    lu_menuitems = menuitems.filter(meal = "Lunch").order_by('-entree__votes')
+    di_menuitems = menuitems.filter(meal = "Dinner").order_by('-entree__votes')
     hallname_dict = {
         "cafe_3": u"Café 3",
         "clark_kerr": "Clark Kerr",
