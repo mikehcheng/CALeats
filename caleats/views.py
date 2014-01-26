00cd11614fb4713bebd8f58a9a073334ea8c494e
Ravi Tadinada <discursor@gmail.com>
2014-01-25 16:44:59 -0800

    Fix voting

--- caleats/views.py
+++ caleats/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
@@ -9,32 +11,35 @@ def index(request):
     return render(request, 'caleats/index.html')
 
 def detail(request, hall):
-    menuitems = MenuItem.objects.filter(hall = hall.lower())
+    hall = hall.lower()
+    menuitems = MenuItem.objects.filter(hall = hall)
     br_menuitems = menuitems.filter(meal = "Breakfast")
     lu_menuitems = menuitems.filter(meal = "Lunch")
     di_menuitems = menuitems.filter(meal = "Dinner")
+    hallname_dict = {
+        "cafe_3": u"Café 3",
+        "clark_kerr": "Clark Kerr",
+        "crossroads": "Crossroads",
+        "foothill": "Foothill"
+        }
     context = {
-    	'hall': hall,
+    	'hall': hallname_dict[hall],
     	'br_menuitems': br_menuitems,
     	'lu_menuitems': lu_menuitems,
-    	'di_menuitems': di_menuitems
+    	'di_menuitems': di_menuitems,
     	}
     return render(request, 'caleats/detail.html', context)
 
 def vote(request):
-    print("bye")
     results = {'success':False}
     if request.method == u'GET':
-    	print("byebye")
         GET = request.GET
         if GET.has_key(u'pk') and GET.has_key(u'vote'):
-            print("byebyebye")
             pk = int(GET[u'pk'])
             vote = GET[u'vote']
             entree = MenuItem.objects.get(pk=pk).entree
             if vote == u"up":
                 entree.votes += 1
-                print("{0}, {1}".format(entree.name, entree.votes))
             elif vote == u"down":
                 entree.votes -= 1
             entree.save()
