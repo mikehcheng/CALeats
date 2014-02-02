6ccd198e613f8f5000b2f028568fbb2ed8e72864
Ravi Tadinada <discursor@gmail.com>
2014-02-01 19:15:45 -0800

    Switch to postgress, add test data

--- CalDining/settings.py
+++ CalDining/settings.py
@@ -58,8 +58,11 @@ WSGI_APPLICATION = 'CalDining.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'caleatsdb',                      
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost'
     }
 }
 
--- caleats/populate.py
+++ caleats/populate.py
@@ -8,13 +8,17 @@ def get_entree_if_exists(itemname):
     except: #DoesNotExist
         entree = Entree(name = itemname)
         entree.save()
+        print("Create Entree: " + itemname)
     return entree
 
-def populate_day():
+def populate_day(url='http://services.housing.berkeley.edu/FoodPro/dining/static/todaysentrees.asp'):
+    print("Starting population")
     MenuItem.objects.all().delete()
+    print("Deleted old MenuItems")
 
-    response = urllib2.urlopen('http://services.housing.berkeley.edu/FoodPro/dining/static/todaysentrees.asp')
+    response = urllib2.urlopen(url)
     html = response.read()
+    print("Read HTML")
 
     #trash.py - go for <!-- -->
     content = BeautifulSoup(html, 'html5lib').find_all(id = 'content')[2].tbody.find_all(
@@ -23,6 +27,7 @@ def populate_day():
     LOCATIONS = {0: "crossroads", 1: "cafe_3", 2: "foothill", 3: "clark_kerr"}
     #FOODTYPE = {u'#800040': 'Vegan', u'#008000': 'Vegetarian', u'#000000': 'Regular'}
     #more than one vegetarian color o__O
+    print("Strained content")
 
     for time in content: #b/l/d
         for l, loc in enumerate(time.find_all('td')): #cr/c3/fh/ck
@@ -38,3 +43,8 @@ def populate_day():
                 ent = get_entree_if_exists(name)
                 newitem = MenuItem(entree = ent, hall = dininghall, meal = meal)
                 newitem.save()
+                print("Create MenuItem: " + name)
+    print("Finished")
+
+def populate_day_test():
+    populate_day('http://web.archive.org/web/20140129140004/http://services.housing.berkeley.edu/FoodPro/dining/static/todaysentrees.asp')
